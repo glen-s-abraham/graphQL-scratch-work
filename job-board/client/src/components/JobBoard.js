29f543e99836630e7a877511a933da1329b93446
glen-s-abraham <glenprojects666@gmail.com>
2022-09-04 19:21:38 +0530

    react client initial rendering

--- job-board/client/src/components/JobBoard.js
+++ job-board/client/src/components/JobBoard.js
@@ -1,12 +1,15 @@
-import JobList from './JobList';
-import { jobs } from '../fake-data';
+import JobList from "./JobList";
+import { getJobs } from "../graphql/queries.js";
+import { useEffect, useState } from "react";
 
 function JobBoard() {
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    getJobs().then(setJobs);
+  }, []);
   return (
     <div>
-      <h1 className="title">
-        Job Board
-      </h1>
+      <h1 className="title">Job Board</h1>
       <JobList jobs={jobs} />
     </div>
   );
