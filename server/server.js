591f9017cc714a4fece33181ace83c485cfe3caf
glen-s-abraham <glenprojects666@gmail.com>
2022-09-04 10:12:46 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const { ApolloServer,gql } = require("apollo-server");
+
+const typeDefs = gql`
+    type Query{
+        greeting:String
+    }
+`;
+
+const resolvers = {
+    Query:{
+        greeting:()=>'Hello world'
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,resolvers
+})
+
+
+server.listen({port:9000}).then(info=>console.log(`Server running on ${info.url}`)).catch(err=>console.log(err));
+
+
